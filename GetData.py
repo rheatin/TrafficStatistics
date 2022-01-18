0dcb6e6a725c0d90b61e232b2f3c2312ed8cabb0
Char <125110985@qq.com>
2022-01-18 15:46:58 +0800

    add a html to show the traffic charts

--- GetData.py
+++ GetData.py
@@ -1,13 +1,57 @@
-import sqlite3
+import http.server
+from mydatabase import operations
+from mypve import pvecommands
 
 print('start')
-conn = sqlite3.connect("traffic.db")
-c = conn.cursor()
+op = operations()
+pve = pvecommands()
+op.recordTraffic(pve.fetchListOfVMsInfoDict())
+# op.selectInfo("select NETIN,NETOUT from traffic where VMID = 105 and DATE = '2022-01-17'")
+rs = op.selectInfo(
+    "select VMID,NAME,DATE,DAILYIN,DAILYOUT from traffic where strftime('%Y-%m-%d', datetime('now'),'-2 days') <= strftime('%Y-%m-%d',DATE)")
+dicList = {}
+for row in rs:
+    if row[1] not in dicList:
+        dicList[row[1]] = {}
+    dicList[row[1]][row[2]] = {'dailyin': row[3], 'dailyout': row[4]}
 
-cursor = c.execute('select VMID,NAME,DATE,NETIN,NETOUT,DAILYIN,DAILYOUT from traffic')
+option = '''
+option = {
+  legend: {},
+  tooltip: {},
+  dataset: {
+    dimensions: ['GB', '2015', '2016', '2017'],
+    source: [
+    ]
+  },
+  xAxis: { type: 'category' },
+  yAxis: {},
+  series: [{ type: 'bar' }, { type: 'bar' }, { type: 'bar' }]
+};
+'''
 
-for row in cursor:
-    print(row[0],row[1],row[2],row[5] / (1024.0 * 1024 * 1024),row[6] / (1024.0 * 1024 * 1024))
+ls = option.split('\n')
+dates = list(list(dicList.values())[0].keys())
 
-conn.commit()
-conn.close()
+for i in range(len(ls)):
+    if 'dimensions' in ls[i]:
+        ls[i] = ls[i].replace('2015', dates[0]).replace(
+            '2016', dates[1]).replace('2017', dates[2])
+    elif 'source: [' in ls[i]:
+        for k, v in dicList.items():
+            string = "{{ GB: '{}', '{}': {}, '{}': {}, '{}': {} }},".format(k, dates[0], v[dates[0]]['dailyout']/(
+                1024 * 1024 * 1024), dates[1], v[dates[1]]['dailyout']/(1024 * 1024 * 1024), dates[2], v[dates[2]]['dailyout']/(1024 * 1024 * 1024))
+            ls.insert(i+1, string)
+
+cont = ''
+with open('index.txt','r') as f:
+    cont = f.read()
+    cont = cont.replace('placeholder','\n'.join(ls))
+    f.close()
+with open('index.html','w+') as f:
+    f.write(cont)
+
+httpd = http.server.HTTPServer(
+    ("localhost", 9000), http.server.SimpleHTTPRequestHandler
+)
+httpd.serve_forever()
--- TrafficStatistics.py
+++ TrafficStatistics.py
@@ -1,147 +1,24 @@
+#cron 59 23 * * *
+
 from concurrent.futures import thread
-import os
-import yaml
-import sqlite3
-import time
 import threading
-from datetime import date, timedelta
+from mydatabase import operations
+from mypve import pvecommands
 
-
-dictsList = []
-def readVMInfo(vmInfo):
-    cmd = "qm status " + vmInfo["VMID"] + " --verbose"
-    p = os.popen(cmd)
-    tmp = p.read()
-    tmp = yaml.safe_load(tmp.replace("\t", "    "))
-    p.close()
-    dictsList.append(tmp)
-    
-
-
-def recordTraffic():
-
-    p = os.popen("qm list")
-    vmListRaw = p.read()
-    p.close()
-
-    # with open("tmp.txt","r") as f:
-    #     vmListRaw = f.read()
-    """ 
-        VMID NAME                 STATUS     MEM(MB)    BOOTDISK(GB) PID       
-        100 openwrt              running    1024               0.00 4027452   
-        101 onething             running    10240            238.47 685861    
-        102 onething-mechanical  running    8192            1024.00 372718    
-        104 home-assistant       running    1536              32.00 1475      
-        105 alpine               running    10240            200.00 1831833   
-    """
-    vmInfosRaw = vmListRaw.split("\n")
-    columns = []
-    vmInfos = []
-    for vmInfoRawString in vmInfosRaw:
-        if vmInfoRawString == "":
-            continue
-        #       100 openwrt              running    1024               0.00 4027452
-        vmInfoList = vmInfoRawString.split(" ")
-        while "" in vmInfoList:
-            vmInfoList.remove("")
-        if "VMID" in vmInfoList:
-            columns = vmInfoList
-        else:
-            vmInfo = {}
-            for i in range(len(vmInfoList)):
-                vmInfo[columns[i]] = vmInfoList[i]
-            vmInfos.append(vmInfo)
-
-    conn = sqlite3.connect("traffic.db")
-
-    threads = []
-    
-    for vmInfo in vmInfos:
-        c = conn.cursor()
-        t = threading.Thread(target=readVMInfo,args=(vmInfo,))
-        t.start()
-        threads.append(t)
-    
-    for t in threads:
-        t.join()
-
-    for tmp in dictsList:
-        vmid = tmp["vmid"]
-        netin = tmp["netin"]
-        netout = tmp["netout"]
-        name = tmp["name"]
-
-        command = """
-            CREATE TABLE IF NOT EXISTS traffic
-                    (VMID INTEGER,
-                    DATE TEXT,
-                    NAME TEXT,
-                    NETIN INTEGER,
-                    NETOUT INTEGER,
-                    DAILYIN INTEGER,
-                    DAILYOUT INTEGER, 
-                    RAW TEXT,
-                    PRIMARY KEY(VMID,DATE));
-            """
-        c.execute(command)
-
-        command = "select NETIN,NETOUT from traffic where strftime('%Y-%m-%d', datetime('now'),'-1 days') = strftime('%Y-%m-%d',DATE) and VMID = {}".format(
-            str(vmid)
-        )
-
-        dailyin = 0
-        dailyout = 0
-
-        cursor = c.execute(command)
-        rs = cursor.fetchone()
-        if rs:
-            netinOfYesterday = rs[0]
-            netoutOfYesterday = rs[1]
-            dailyin = netin - int(netinOfYesterday)
-            dailyout = netout - int(netoutOfYesterday)
-
-        else:
-            # INSERT FAKE NETIN AND NETOUT FOR YESTERDAY TO CALCULATE THE DAILYIN AND DAILYOUT
-            yesterday = (date.today() + timedelta(days=-1)).strftime("%Y-%m-%d")
-            command = 'INSERT INTO traffic (VMID,DATE,NAME,NETIN,NETOUT,DAILYIN,DAILYOUT,RAW) \
-                VALUES ({},"{}","{}",{},{},{},{},"{}")'.format(
-                str(vmid),
-                yesterday,
-                str(name),
-                str(netin),
-                str(netout),
-                str(dailyin),
-                str(dailyout),
-                "FAKE",
-            )
-            c.execute(command)
-
-        print(
-            vmid,
-            time.strftime("%Y-%m-%d", time.localtime()),
-            netin,
-            netout,
-            dailyin,
-            dailyout,
-        )
-        command = 'REPLACE INTO traffic (VMID,DATE,NAME,NETIN,NETOUT,DAILYIN,DAILYOUT,RAW) \
-                VALUES ({},"{}","{}",{},{},{},{},"{}")'.format(
-            str(vmid),
-            time.strftime("%Y-%m-%d", time.localtime()),
-            str(name),
-            str(netin),
-            str(netout),
-            str(dailyin),
-            str(dailyout),
-            str(tmp),
-        )
-        c.execute(command)
-        conn.commit()
-
+times = 20
 
 def startRecording():
-    recordTraffic()
-    threading.Timer(10, startRecording).start()
+    global times
+    if times > 0:
+        op = operations()
+        pve = pvecommands()
 
+        # 101,onething,2022-01-18,2542608095173,7000098385919,53367164064,71000199441
+        # fakeList = [{'vmid':101,'name':'onething','netin':2542608095173,'netout':7000098385919}]
+        # op.recordTraffic(fakeList)
+        op.recordTraffic(pve.fetchListOfVMsInfoDict())
+        times = times - 1
+        threading.Timer(3, startRecording).start()
 
-startRecording()
+if __name__ == '__main__':
+    startRecording()
